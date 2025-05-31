42f9b12f09e0b7b1141c7fdadc9e2a812f66dbca
nika <fantozy.v2@gmail.com>
2025-06-01 02:12:45 +0400

    added EmailVerificationCode model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
+from datetime import timedelta
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+
 from config.model_utils.models import TimeStampedModel 
 
 class User(AbstractUser, TimeStampedModel):
@@ -9,4 +13,10 @@ class User(AbstractUser, TimeStampedModel):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'phone_number']
 
+class EmailVerificationCode(TimeStampedModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    def is_expired(self):
+        return timezone.now() > self.created_at + timedelta(minutes=10)
